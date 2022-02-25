c5a09484505f4dfe43f6907622a399acdce1ffe1
Ashley Mattam <amattam427@gmail.com>
2022-02-25 02:33:55 -0500

    commit tree d3ff33583d637bddc08ca11422535ec7a56ae727

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -6,6 +6,20 @@ class DogHousesController < ApplicationController
     render json: dog_house
   end
 
+  #unneeded code::
+  # #finding a specific dog house and looking at all of the reviews for that specific doghouse
+  # def reviews_index 
+  #   dog_house = DogHouse.find(params[:dog_house_id])
+  #   reviews = dog_house.reviews 
+  #   render json: reviews, include: :dog_house
+  # end
+
+  # #reading one specific review and showing which doghouse that review belongs to.
+  # def review
+  #   review= Review.find(params[:id])
+  #   render json: review, include: :dog_house
+  # end
+
   private
 
   def render_not_found_response
